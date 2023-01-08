efd8ad3fcc57d36cb33bb718a771c3b1034edc3e
nganho <nganho2251@gmail.com>
2023-01-08 17:55:06 +0700

    hi

--- src/routes/nhanvien.js
+++ src/routes/nhanvien.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 const NhanVienController = require('../app/controllers/NhanVienController')
 
-
+/*
 router.get('/', NhanVienController.nv)
 router.get('/hd', NhanVienController.Xem_HD)
 router.get('/dt1', NhanVienController.Xem_DT_Dang_QL)
 router.get('/dt2', NhanVienController.Xem_DT_Vua_QL)
-
+*/
 module.exports = router
