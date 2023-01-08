3cc22f5ba93c55e6bb5569a4a2293b242ab0b910
Oxaloala <nguyenngoc2001.22@gmail.com>
2023-01-08 17:41:27 +0700

    n1

--- src/routes/nhanvien.js
+++ src/routes/nhanvien.js
@@ -2,4 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const NhanVienController = require('../app/controllers/NhanVienController')
+
+
+router.get('/', NhanVienController.nv)
+router.get('/hd', NhanVienController.Xem_HD)
+router.get('/dt1', NhanVienController.Xem_DT_Dang_QL)
+router.get('/dt2', NhanVienController.Xem_DT_Vua_QL)
+
 module.exports = router
